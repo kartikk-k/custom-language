3ec38f8f164eee6a83604d6d03ea23961d43c50a
Kartik Khorwal <hellokartikk@gmail.com>
2023-09-25 02:05:41 +0530

    parse arthemetic operations (+, -, *, /, %)

--- compiler/lexer.ts
+++ compiler/lexer.ts
@@ -66,7 +66,7 @@ export function tokenize(sourceCode: string): Token[] {
         } else if (src[0] == ")") {
             tokens.push(token(src.shift(), TokenType.CloseParenthesis));
 
-        } else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+        } else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/" || src[0] == "%") {
             tokens.push(token(src.shift(), TokenType.BinaryOperator));
 
         } else if (src[0] == "=") {
--- compiler/parser.ts
+++ compiler/parser.ts
@@ -21,6 +21,16 @@ export default class Parser {
         return previous;
     }
 
+    private expect(type: TokenType, err: any) {
+        const previous = this.tokens.shift() as Token;
+        if (!previous || previous.type != type) {
+            console.log("Parser Error:\n", err, previous, "Expecting: ", type)
+            Deno.exit();
+        }
+
+        return previous;
+    }
+
     public produceAST(sourceCode: string): Program {
         this.tokens = tokenize(sourceCode);
         const program: Program = {
@@ -42,8 +52,61 @@ export default class Parser {
     }
 
     private parse_expression(): Expression {
-        return this.parse_primary_expression();
+        // return this.parse_primary_expression();
+        return this.parse_additive_expression();
     }
+
+    // ex: (10 + 5) - 4
+    private parse_additive_expression(): Expression {
+        let left = this.parse_multiplicitave_expression();
+
+        // parse operator
+        while (this.current_token().value == "+" || this.current_token().value == "-") {
+            const operator = this.eat().value;
+            const right = this.parse_multiplicitave_expression();
+            left = {
+                kind: "BinaryExpression",
+                left,
+                right,
+                operator
+            } as BinaryExpression
+        }
+
+        return left;
+    }
+
+    private parse_multiplicitave_expression(): Expression {
+        let left = this.parse_primary_expression();
+
+        // parse operator
+        while (this.current_token().value == "/" || this.current_token().value == "*" || this.current_token().value == "%") {
+            const operator = this.eat().value;
+            const right = this.parse_primary_expression();
+            left = {
+                kind: "BinaryExpression",
+                left,
+                right,
+                operator
+            } as BinaryExpression
+        }
+
+        return left;
+    }
+
+
+
+    // Orders os prescidence
+
+    // Assignment Expression
+    // Member Expression
+    // Function Call
+    // Logical Expression
+    // Comparision Expression
+    // Addictive Expression
+    // Multiplicitave Expression
+    // Unary Expression
+    // Primary Expression
+
 
     private parse_primary_expression(): Expression {
         const token = this.current_token().type;
@@ -54,6 +117,13 @@ export default class Parser {
 
             case TokenType.Number:
                 return { kind: "NumericLiteral", value: parseFloat(this.eat().value) } as NumericLiteral;
+
+            case TokenType.OpenParenthesis: {
+                this.eat(); // remove the opne parenthesis and get value
+                const value = this.parse_expression();
+                this.expect(TokenType.CloseParenthesis, "Unexpected token found inside parenthesised expression. Expected closing parenthesis",); // closing parenthesis
+                return value;
+            }
 
             default:
                 console.error("Unexpected token found during parsing!", this.current_token())
